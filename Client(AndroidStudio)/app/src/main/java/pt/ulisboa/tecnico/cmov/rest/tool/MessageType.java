e73ee294e6c2c372339eabfbbea6b50002cc217e
Aquilino <akilinosilva@gmail.com>
2018-05-09 18:43:43 +0100

    Created folder for the Project Code

--- Client(AndroidStudio)/app/src/main/java/pt/ulisboa/tecnico/cmov/rest/tool/MessageType.java
+++
@@ -1,9 +0,0 @@
-package pt.ulisboa.tecnico.cmov.rest.tool;
-
-public enum MessageType {
-    CREATE,
-    LOGIN,
-    LOGOUT,
-    GETPROFILE,
-    SENDPROFILE
-}
--- Server(Eclipse)/src/main/java/ist/meic/cmu/server/tool/StringParser.java
+++
@@ -1,9 +0,0 @@
-package ist.meic.cmu.server.tool;
-
-public class StringParser {
-    public static String[] getProperty(String input){
-        String tmp=input.substring(input.indexOf("[") + 1, input.indexOf("]"));
-        String[] output=tmp.split(",");
-        return output;
-    }
-}
